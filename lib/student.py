e8ca7751630e34fb2bc7613bd88a85e9b11d2924
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-12-02 21:36:15 +0300

    inheritance

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, added_know):
+        self.knowledge.append(added_know)
+
+student = Student("jfdj", "djfhiu")
+student.learn("EUGH, EEUUGGHH!!")
+
+print(student.knowledge)
